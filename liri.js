7abf4f19ff581225008938d20ef553fc29753c4b
ayanna kosoko <ayannakosoko@gmail.com>
2018-07-25 19:47:54 -0400

    okay

--- liri.js
+++ liri.js
@@ -1,185 +1,106 @@
-require("dotenv").config();
+require('dotenv').config();
 
-var action = process.argv[2];
-// var value = process.argv[3]; (THIS ONLY TAKES IN ONE NODE ARGUMENT FOR THE 3RD NODE SO ADDED CODE BELOW)
+var keys = require('./keys.js');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require('fs');
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+var command = process.argv[2];
 
-// STORE ALL OF THE ARGUMENTS IN AN ARRAY
-var nodeArgs = process.argv;
-
-// CREATE AN EMPTY VARIABLE FOR HOLDING USERS INPUT VALUE
 var value = "";
 
-// LOOPS THROUGH ALL THE WORDS IN THE NODE ARGUMENT 
-// DO A LOOP TO GET EVERYTHING AFTER THE INDEX OF 2 NODE ARGUMENT
-for (var i = 3; i < nodeArgs.length; i++) {
-
-    if (i > 3 && i < nodeArgs.length) {
-
-        value = value + "+" + nodeArgs[i];
-
-    } else {
-
-        value = value + nodeArgs[i];
+for (var x = 3; x < process.argv.length; x++) {
+    value += process.argv[x] 
+    if(x != process.argv.length) {
+        value = value + "+"
     }
 }
 
-// SWITCH STATEMENT FOR RUNNING DIFFERENT APPS 
-// THIS WILL DIRECT WHICH FUNCTION TO RUN
-switch (action) {
+switch (command) {
     case 'my-tweets':
-        twitter();
+        getTweets();
         break;
-
     case 'spotify-this-song':
-        spotify();
+        spotifySong(value);
         break;
-
     case 'movie-this':
-        imdb();
+        OMDB(value);
+        break;
+    case 'do-what-it-says':
+        whatItSays();
+        break;
+    default:
+        console.log('You entered an unknown command');
         break;
 }
 
-
-
-//TWITTER
-
-function twitter() {
-    var fs = require('fs');
-    var twitterKey = require('./keys.js');
-    var Twitter = require('twitter');
-    var client = new Twitter(twitterKey.twitterKeys);
-
-    var params = { screen_name: value, count: 20 };
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (!error) {
-
-            console.log("tweets:");
-
-            for (var i = 0; i < tweets.length; i++) {
-
-                console.log("_____________________________________________");
-                console.log("Tweeted on: " + tweets[i].created_at);
+function getTweets() {
+	client.get('statuses/user_timeline', {
+		status: "I am a tweet"
+	}, function(error, tweets, response) {
+		if (!error) {
+			for (var i = 0; i < tweets.length; i++) {
                 console.log(tweets[i].text);
-
-            }
-        }
-    });
+                console.log("")
+				fs.appendFile('log.txt', "my-tweets: " + tweets[i].text + "\n", function(err) {
+					if (err) throw err;
+				});
+			}
+		}
+	});
 }
 
-
-
-
-
-//SPOTIFY
-
-function spotify() {
-
-    if (value != false) {
-        var spotify = require('node-spotify-api');
-
-        spotify.search({
-            type: 'track',
-            query: value + '&limit=1&'
-        }, function(error, data) {
-            if (error) {
-                console.log('Error occurred: ' + error);
-                return;
-            }
-            console.log("song " + value + ".");
-            console.log("track: " + data.tracks.items[0].name);
-            console.log("artist: " + data.tracks.items[0].artists[0].name);
-            console.log("url: " + data.tracks.items[0].preview_url);
-        });
-    } else {
-        {
-            var spotify = require('spotify');
-
-            spotify.search({
-                type: 'track',
-                query: 'ace+of+base+sign' + '&limit=1&'
-            }, function(error, data) {
-                if (error) {
-                    console.log('Error occurred: ' + error);
-                    return;
-                }
-                
-                console.log("track: " + data.tracks.items[0].name);
-                console.log("artist: " + data.tracks.items[0].artists[0].name);
-                console.log("url: " + data.tracks.items[0].preview_url);
-            });
-        }
-
-    }
+function spotifySong(song) {
+	song = song || 'The Sign by Ace of Base';
+	spotify.search({
+		type: 'track',
+		query: song,
+		limit: 1
+	}, function(error, data) {
+		if (error) {
+			return console.log('Error Occurred: ' + error);
+		}
+        console.log('Song’s Name: ' + data.tracks.items[0].name);
+        console.log("")
+        console.log('Artist(s): ' + data.tracks.items[0].artists[0].name);
+        console.log("")
+        console.log('Spotify Link Preview: ' + data.tracks.items[0].preview_url);
+        console.log("")
+		console.log('Song’s Name: ' + data.tracks.items[0].album.name);
+	});
 }
 
-
-
-
-
-//IMDB
-
-function imdb() {
-
-    // INCLUDE THE REQUEST NPM PACKAGE (DONT FORGET TO RUN "NPM INSTALL REQUEST" IN THIS FOLDER FIRST!) 
-    var request = require('request');
-
-    // RUN A REQUEST TO THE OMDB API WITH THE MOVIE SPECIFIED (CAN BE MULTI WORD MOVIES)
-    request('http://www.omdbapi.com/?t=' + value + '&y=&plot=short&tomatoes=true&r=json', function(error, response, body) {
-
-        // IF THE REQUEST IS SUCCESFUL (i.e. IF THE RESPONSE STATUS CODE IS 200)
-        //if (!error && response.statusCode == 200) {
-        if (value != false) {
-
-            // PARSE THE BODY OF THE SITE WITH THE FOLLWING INFORMATION 
-            console.log("======================================================================");
-            console.log("The movie's name is: " + JSON.parse(body).Title);
-            console.log("");
-            console.log("The movie was released in: " + JSON.parse(body).Year);
-            console.log("");
-            console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-            console.log("");
-            console.log("This movie was produced in the: " + JSON.parse(body).Country);
-            console.log("");
-            console.log("The language for this movie is in: " + JSON.parse(body).Language);
-            console.log("");
-            console.log("The movie's Plot: " + JSON.parse(body).Plot);
-            console.log("");
-            console.log("The movie's Actor's: " + JSON.parse(body).Actors);
-            console.log("");
-            console.log("");
-            console.log("The Rotten Tomato rating is: " + JSON.parse(body).tomatoRating);
-            console.log("");
-            console.log("The Rotten Tomato URL is: " + JSON.parse(body).tomatoURL);
-            console.log("");
-        } else {
-
-            //IF THERE IS MOVIE NETERED BY THE USER IT WILL DEFAULT TO THE MOVIE PRELAODED HERE MR. NOBODY.     
-            var request = require('request');
-
-            // RUN A REQUEST TO THE OMDB API WITH THE MOVIE SPECIFIED (CAN BE MULTI WORD MOVIES)
-            request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&tomatoes=true&r=json', function(error, response, body) {
-
-                // PARSE THE BODY OF THE SITE WITH THE FOLLWING INFORMATION 
-                console.log("======================================================================");
-                console.log("The movie's name is: " + JSON.parse(body).Title);
-                console.log("");
-                console.log("The movie was released in: " + JSON.parse(body).Year);
-                console.log("");
-                console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-                console.log("");
-                console.log("This movie was produced in the: " + JSON.parse(body).Country);
-                console.log("");
-                console.log("The language for this movie is in: " + JSON.parse(body).Language);
-                console.log("");
-                console.log("The movie's Plot: " + JSON.parse(body).Plot);
-                console.log("");
-                console.log("The movie's Actor's: " + JSON.parse(body).Actors);
-                console.log("");
-                console.log("The Rotten Tomato rating is: " + JSON.parse(body).tomatoRating);
-                console.log("");
-                console.log("The Rotten Tomato URL is: " + JSON.parse(body).tomatoURL);
-                console.log("");
-            });
-        }
-    });
+function OMDB(movie) {
+	movie = movie || 'Mr. Nobody';
+	request('http://www.omdbapi.com/?t=' + value + '&y=&plot=short&apikey=trilogy', function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+            console.log('Movie: ' + JSON.parse(body).Title);
+            console.log("")
+            console.log('Year: ' + JSON.parse(body).Year);
+            console.log("")
+            console.log('IMDB Rating: ' + JSON.parse(body).imdbRating);
+            console.log("")
+            console.log('Rotten Tomatoes Rating: ' + JSON.parse(body).Ratings[0].Value);
+            console.log("")
+            console.log('Country: ' + JSON.parse(body).Country);
+            console.log("")
+            console.log('Language: ' + JSON.parse(body).Language);
+            console.log("")
+            console.log('Plot: ' + JSON.parse(body).Plot);
+            console.log("")
+			console.log('Actors: ' + JSON.parse(body).Actors);
+		}
+	});
 }
+
+function whatItSays() {
+	fs.readFile("random.txt", "utf8", function(error, data) {
+		if (error) {
+			return console.log("Error:" + error);
+		}
+		liri((data.split(",")[0]), (data.split(",")[1]));
+	});
+}
+
